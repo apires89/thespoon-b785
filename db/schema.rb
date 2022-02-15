73437d4b8237ab4e3ab526cffbefdfb331fd900d
andre <andrecabralpires@gmail.com>
2022-02-15 09:27:38 +0100

    model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_15_081228) do
+ActiveRecord::Schema.define(version: 2022_02_15_081653) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
+    t.integer "phone_number"
   end
 
 end
